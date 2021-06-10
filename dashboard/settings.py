1b479814bbb97fa0fffc06ebbc541a46c3c6606d
parthishere <parthishere1234@gmail.com>
2021-06-10 21:05:55 +0530

    chnages

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
